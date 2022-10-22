810af2fb84659fc0afa3a6b58505f440165fba23
Yunus Emre <111645885+yunussturker@users.noreply.github.com>
2022-10-22 21:16:59 +0300

    Create Homework1.sql

---
+++ Homework1.sql
@@ -0,0 +1,18 @@
+--Answer 1
+SELECT title, description FROM film;
+
+--Answer 2
+SELECT * FROM film
+WHERE length > 60 AND length < 75;
+
+--Answer 3
+SELECT * FROM film
+WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
+
+--Answer 4
+SELECT first_name, last_name FROM customer
+WHERE first_name = 'Mary';
+
+--Answer 5
+SELECT * FROM film
+WHERE length <= 50 AND NOT(rental_rate = 2.99 OR rental_rate = 4.99);
