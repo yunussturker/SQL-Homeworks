e8b1588bf4072065833050a8ce2d27e7f34c03c6
Yunus Emre <111645885+yunussturker@users.noreply.github.com>
2022-10-29 11:09:50 +0300

    Create Homework12.sql

---
+++ Homework12.sql
@@ -0,0 +1,18 @@
+--1
+SELECT * FROM film
+WHERE length > (
+SELECT AVG(length) FROM film);
+
+--2
+SELECT COUNT(*) FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+--3
+SELECT * FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)
+AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+--4
+SELECT customer_id, COUNT(customer_id) FROM payment
+GROUP BY customer_id
+ORDER BY COUNT(customer_id) DESC;
